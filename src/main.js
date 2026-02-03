cea7bf19235b7775c9c7f178dd125eb20aae022d
ivana-koceva <ivana.koceva10@gmail.com>
2026-02-03 10:55:23 +0100

    added pages inside scrapbook

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { objectGroup } from 'three/src/nodes/TSL.js';
 
+// defaults for threejs
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const canvas = document.querySelector("canvas.threejs");
+const canvas = document.querySelector("#main");
 const renderer = new THREE.WebGLRenderer({canvas});
 
 camera.position.z = 5;
@@ -11,7 +13,9 @@ camera.position.z = 5;
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 
+scene.add(new THREE.AmbientLight(0xffffff, 0.6));
 
+// draw rounded rectangle in 2d
 function roundedRectShape(width, height, radius) {
   const shape = new THREE.Shape();
 
@@ -28,36 +32,49 @@ function roundedRectShape(width, height, radius) {
   return shape;
 }
 
+// add geometry to object
+function geometry(object, depth, bevel, curves) {
+  return new THREE.ExtrudeGeometry(object, {
+    depth: depth,
+    bevelEnabled: bevel,
+    curveSegments: curves
+  }).center();
+}
+
+// add repeating texture to object
+function texture(path) {
+  const texture = new THREE.TextureLoader().load(path);
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set(1, 1);
+
+  return new THREE.MeshStandardMaterial({
+  map: texture,
+  });
+}
+
+// add objects
 const shape = roundedRectShape(4, 3, 0.15);
+const shape2 = roundedRectShape(1.9, 2.8, 0.15);
 
-const geometry = new THREE.ExtrudeGeometry(shape, {
-  depth: 0.15,
-  bevelEnabled: false,
-  curveSegments: 12
-});
+const cover = new THREE.Mesh(geometry(shape, 0.15, false, 12), texture("/images/leather.jpg"));
+const pageOne = new THREE.Mesh(geometry(shape2, 0.05, false, 12), texture("/images/paper2.jpg"));
+pageOne.position.x = 0.9;
+pageOne.position.z = 0.1;
 
-geometry.center();
-
-
-const leatherTexture = new THREE.TextureLoader().load('/images/leather.jpg');
-leatherTexture.wrapS = leatherTexture.wrapT = THREE.RepeatWrapping;
-leatherTexture.repeat.set(1, 1);
-
-
-const material = new THREE.MeshStandardMaterial({
-  map: leatherTexture,
-});
-
-const cover = new THREE.Mesh(geometry, material);
+const pageTwo = new THREE.Mesh(geometry(shape2, 0.05, false, 12), texture("/images/paper2.jpg"));
+pageTwo.position.x = -0.9;
+pageTwo.position.z = 0.1;
 scene.add(cover);
+scene.add(pageOne);
+scene.add(pageTwo);
 
-scene.add(new THREE.AmbientLight(0xffffff, 0.6));
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
 const spaceTexture = new THREE.TextureLoader().load('/images/sky.jpg');
 scene.background = spaceTexture;
 
+// update animation each second
 function animate() {
   requestAnimationFrame(animate);
   controls.update();
